ed75ab25fe7c950bd8162937a83696bdfb1f384c
hermes829 <hermes829@gmail.com>
2016-09-27 21:47:06 -0700

    sep out appendix

--- code/runResults/latSpaceViz.R
+++ code/runResults/latSpaceViz.R
@@ -7,7 +7,6 @@ tmp = as.matrix(read.table(file = paste0(dataPath, "climate0205-collab.csv"),
 	header = TRUE, row.names = 1, sep = ";"))
 load(paste0(dataPath, 'data.rda'))
 rownames(Y) = colnames(Y) = rownames(tmp)
+
 load(paste0(resultsPath, 'ameFitSR_2.rda'))
-
 circplot(Y=Y, U=ameFit$'U', V=ameFit$'V')
-
