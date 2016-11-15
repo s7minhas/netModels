b1aa2db1bf412622744ac1494d3eb042051a786a
hermes829 <hermes829@gmail.com>
2016-11-15 01:41:08 -0800

    instruc mods

--- replArchive/helpers/paths.R
+++ replArchive/helpers/paths.R
@@ -3,5 +3,5 @@ dataPath = paste0(dPath, "data/")
 resultsPath = paste0(dPath, 'rdaResults/')
 graphicsPath = paste0(dPath, 'summResults/')
 
-gPath = 'netModels/'
-funcPath = paste0(gPath, 'code/helpers/')
+gPath = NULL
+funcPath = paste0(gPath, 'helpers/')
