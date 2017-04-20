8fcfb6b6b6ddc3405c5a2ec8bc453648f59122f9
s7minhas <hermes829@gmail.com>
2017-04-20 10:40:32 -0700

    more sim stats

--- code/simulations/ameSimTest_Summ.R
+++ code/simulations/ameSimTest_Summ.R
@@ -4,6 +4,7 @@ rm(list=ls())
 
 #helper fns
 checkOverlap = function(x,y){ x[1] <= y[3] && y[1] <= x[3] }
+checkCoverage = function(x,y){ y[2]>=x[1] & y[2]<=x[3] }
 ######
 
 ######
@@ -12,21 +13,43 @@ dataPath = paste0(dPath, "data/")
 resultsPath = paste0(dPath, 'rdaResults/')
 graphicsPath = paste0(dPath, 'summResults/')
 
-load(paste0(resultsPath, 'ameSimTest_100.rda'))
+# load(paste0(resultsPath, 'ameSimTest_100.rda'))
+load(paste0(resultsPath, 'ameSimTest.rda')) # 1k
 ######
 
 ######
 # 
 vars = c('nfit0','nfit1','bfit0', 'bfit1')
-summ = matrix(NA,nrow(NFITO),4, dimnames=list(NULL,vars))
+summOverlap = matrix(NA,nrow(NFITO),length(vars), dimnames=list(NULL,vars))
+summCoverage = matrix(NA,nrow(NFITO),length(vars), dimnames=list(NULL,vars))
 
+vars = c('nfit0','nfit1', 'nfitO', 'bfit0', 'bfit1', 'bfitO')
+summWidth = matrix(NA,nrow(NFITO),length(vars), dimnames=list(NULL,vars))
+
+# lazy iterate
 for(ii in 1:nrow(NFITO)){
-	summ[ii,'nfit1'] = checkOverlap(NFIT1[ii,],NFITO[ii,])
-	summ[ii,'nfit0'] = checkOverlap(NFIT0[ii,],NFITO[ii,])
+	# do conf ints overlap
+	summOverlap[ii,'nfit1'] = checkOverlap(NFIT1[ii,],NFITO[ii,])
+	summOverlap[ii,'nfit0'] = checkOverlap(NFIT0[ii,],NFITO[ii,])
+	summOverlap[ii,'bfit1'] = checkOverlap(BFIT1[ii,],BFITO[ii,])
+	summOverlap[ii,'bfit0'] = checkOverlap(BFIT0[ii,],BFITO[ii,])
 
-	summ[ii,'bfit1'] = checkOverlap(BFIT1[ii,],BFITO[ii,])
-	summ[ii,'bfit0'] = checkOverlap(BFIT0[ii,],BFITO[ii,])
+	# do conf ints cover oracle mu
+	summCoverage[ii,'nfit1'] = checkCoverage(NFIT1[ii,],NFITO[ii,])
+	summCoverage[ii,'nfit0'] = checkCoverage(NFIT0[ii,],NFITO[ii,])
+	summCoverage[ii,'bfit1'] = checkCoverage(BFIT1[ii,],BFITO[ii,])
+	summCoverage[ii,'bfit0'] = checkCoverage(BFIT0[ii,],BFITO[ii,])	
+
+	# conf int width
+	summWidth[ii,'nfit0'] = NFIT0[ii,3] - NFIT0[ii,1]
+	summWidth[ii,'nfit1'] = NFIT1[ii,3] - NFIT1[ii,1]
+	summWidth[ii,'nfitO'] = NFITO[ii,3] - NFITO[ii,1]
+	summWidth[ii,'bfit0'] = BFIT0[ii,3] - BFIT0[ii,1]
+	summWidth[ii,'bfit1'] = BFIT1[ii,3] - BFIT1[ii,1]
+	summWidth[ii,'bfitO'] = BFITO[ii,3] - BFITO[ii,1]		
 }
 
-apply(summ,2,mean)
+cbind(apply(summOverlap,2,mean))
+cbind(apply(summCoverage, 2, mean))
+cbind(apply(summWidth, 2, mean))
 ######
