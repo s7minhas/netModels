752c71f713db8a4d94fc98c4bef740a767110dc8
hermes829 <hermes829@gmail.com>
2017-04-25 05:43:23 -0600

    update sims

--- code/simulations/ameSimTest_Summ.R
+++ code/simulations/ameSimTest_Summ.R
@@ -3,7 +3,7 @@ rm(list=ls())
 # libs
 
 #helper fns
-checkOverlap = function(x,y){ x[1] <= y[3] && y[1] <= x[3] }
+checkCoverage = function(x){ x[1]<=1 & x[3]>=1 }
 ######
 
 ######
@@ -12,21 +12,36 @@ dataPath = paste0(dPath, "data/")
 resultsPath = paste0(dPath, 'rdaResults/')
 graphicsPath = paste0(dPath, 'summResults/')
 
-load(paste0(resultsPath, 'ameSimTest_100.rda'))
+# load(paste0(resultsPath, 'ameSimTest_100.rda'))
+load(paste0(resultsPath, 'ameSimTest.rda')) # 1k
 ######
 
 ######
 # 
-vars = c('nfit0','nfit1','bfit0', 'bfit1')
-summ = matrix(NA,nrow(NFITO),4, dimnames=list(NULL,vars))
+vars = c('nfit0','nfit1', 'nfitO', 'bfit0', 'bfit1', 'bfitO')
+summCoverage = matrix(NA,nrow(NFITO),length(vars), dimnames=list(NULL,vars))
+summWidth = matrix(NA,nrow(NFITO),length(vars), dimnames=list(NULL,vars))
 
+# lazy iterate
 for(ii in 1:nrow(NFITO)){
-	summ[ii,'nfit1'] = checkOverlap(NFIT1[ii,],NFITO[ii,])
-	summ[ii,'nfit0'] = checkOverlap(NFIT0[ii,],NFITO[ii,])
 
-	summ[ii,'bfit1'] = checkOverlap(BFIT1[ii,],BFITO[ii,])
-	summ[ii,'bfit0'] = checkOverlap(BFIT0[ii,],BFITO[ii,])
+	# do conf ints cover oracle mu
+	summCoverage[ii,'nfit1'] = checkCoverage(NFIT1[ii,])
+	summCoverage[ii,'nfit0'] = checkCoverage(NFIT0[ii,])
+	summCoverage[ii,'nfitO'] = checkCoverage(NFITO[ii,])	
+	summCoverage[ii,'bfit1'] = checkCoverage(BFIT1[ii,])
+	summCoverage[ii,'bfit0'] = checkCoverage(BFIT0[ii,])	
+	summCoverage[ii,'bfitO'] = checkCoverage(BFITO[ii,])		
+
+	# conf int width
+	summWidth[ii,'nfit0'] = NFIT0[ii,3] - NFIT0[ii,1]
+	summWidth[ii,'nfit1'] = NFIT1[ii,3] - NFIT1[ii,1]
+	summWidth[ii,'nfitO'] = NFITO[ii,3] - NFITO[ii,1]
+	summWidth[ii,'bfit0'] = BFIT0[ii,3] - BFIT0[ii,1]
+	summWidth[ii,'bfit1'] = BFIT1[ii,3] - BFIT1[ii,1]
+	summWidth[ii,'bfitO'] = BFITO[ii,3] - BFITO[ii,1]		
 }
 
-apply(summ,2,mean)
+cbind(apply(summCoverage, 2, mean))
+cbind(apply(summWidth, 2, mean))
 ######
