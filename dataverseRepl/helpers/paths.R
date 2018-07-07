f43be4a030f64dbfa5cf39b85996a3ea71f34850
s7minhas <s7.minhas@gmail.com>
2018-07-06 23:45:53 -0400

    simplify repl archive for dataverse

---
+++ dataverseRepl/helpers/paths.R
@@ -0,0 +1,7 @@
+dPath = '~/Dropbox/Research/netModels/'
+dataPath = paste0(dPath, "data/")
+resultsPath = paste0(dPath, 'rdaResults/')
+graphicsPath = paste0(dPath, 'summResults/')
+
+gPath = '~/Research/netModels/'
+funcPath = paste0(gPath, 'code/helpers/')
---
+++ dataverseRepl/runAmen.R
@@ -0,0 +1,35 @@
+rm(list=ls())
+seed <- 12345
+set.seed(seed)
+source('~/Research/netModels/code/helpers/paths.R')
+source(paste0(funcPath, 'functions.R'))
+
+# install amen from s7minhas/github
+loadPkg('devtools') ;
+if(!'amen' %in% installed.packages()[,1]){
+	devtools::install_github('s7minhas/amen')}
+library(amen)
+
+# load data
+load(paste0(dataPath, 'data.rda'))
+
+# mcmc params
+imps = 100000
+brn = 50000
+ods = 10
+latDims = 0:4
+
+# Run amen in parallel
+loadPkg(c('doParallel', 'foreach'))
+cl=makeCluster(4) ; registerDoParallel(cl)
+foreach(ii=1:length(latDims), .packages=c("amen")) %dopar% {
+	
+	ameFit = ameTest(Y=Y, Xdyad=Xd, Xrow=Xs, Xcol=Xr, 
+		model='bin', symmetric=FALSE, R=latDims[ii], 
+		nscan=imps, seed=seed, burn=brn, odens=ods, 
+		rvar=TRUE, cvar=TRUE,  # row and column effects, standard in srm
+		plot=FALSE, print=FALSE) 
+	
+	save(ameFit, file=paste0(resultsPath, 'ameFitSR_', latDims[ii], '.rda'))
+}
+stopCluster(cl)
